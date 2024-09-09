e259431a41a55deb252925875fb61b9853b7d2fb
issaccheung <issaccheung@outlook.com>
2024-09-09 18:53:53 -0400

    Completed Reduce

---
+++ src/Reduce.java
@@ -0,0 +1,15 @@
+class Reduce {
+    public static void main(String[] args) {
+        int count=0;
+        int n=100;
+        while (n > 0){
+            if (n % 2 == 0){
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
